202dbf53694e9c925d8dba5032b99e6ad453d5a9
ravitejashivuni210-star <raviteja.shivuni210@gmail.com>
2026-04-18 11:09:11 +0530

    Update Var_let_const.js with latest changes

---
+++ JAVA_concepts/Var_let_const.js
@@ -0,0 +1,20 @@
+var a =10;
+console.log(a);
+function printhello()
+{
+    console.log("Hello world");
+    var a=30;
+    console.log(a);
+}
+printhello();
+console.log(a);
+
+let PI =3.14;
+console.log(PI);
+PI=3.1415;
+console.log(PI);
+
+function SAM(){
+    console.log("SAM is cute");
+}
+SAM();
